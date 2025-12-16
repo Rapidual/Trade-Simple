250e240026961851f083513069ddf237d2731df4
Thomas Peters <thomas@rapidual.com>
2025-12-16 09:34:53 -0800

    Add files via upload

--- Trade_SimpleApp.swift
+++ Trade_SimpleApp.swift
@@ -10,7 +10,14 @@ import Observation
 
 @main
 struct Trade_SimpleApp: App {
-    @State private var store = LiveDataStore(provider: AlphaVantageProvider())
+    @State private var store: LiveDataStore = {
+        #if targetEnvironment(simulator)
+        // In Simulator, use a provider that does not require an API key.
+        return LiveDataStore(provider: YahooFinanceProvider())
+        #else
+        return LiveDataStore(provider: MassiveMarketDataProvider())
+        #endif
+    }()
 
     var body: some Scene {
         WindowGroup {
